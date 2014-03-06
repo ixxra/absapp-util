84ee1e9991f5ad1e247a25f5a8ec944b1e63f007
ixxra <garciamx@gmail.com>
2014-03-06 10:13:38 -0600

    authors xml export

--- node/authors.js
+++ node/authors.js
@@ -29,29 +29,33 @@ function tokenize(author)
 
 
 console.log('<?xml version="1.0" encoding="UTF-8"?>');
-console.log('<!DOCTYPE users SYSTEM "users.dtd">');
+console.log('<!DOCTYPE users SYSTEM "http://absapp.freeserver.me/ojs/plugins/importexport/users/users.dtd">');
 console.log('<users>');
 
 abs.forEach(function(issue) {
     issue.contents.forEach(function(article) {
         var authors = article.authors.split(/,/);
         authors[0] = authors[0].slice(8);
-        authors.forEach(function (author) {
-            console.log('  <user>');
-            var tokens = tokenize(author.trim());
-            console.log('    <first_name>');
-            console.log('      ', tokens.first_name);
-            console.log('    </first_name>');
-            if (tokens.middle_name){
-                console.log('    <middle_name>');
-                console.log('      ', tokens.middle_name);
-                console.log('    </middle_name>');
-            }
-            console.log('    <last_name>');
-            console.log('      ',tokens.last_name);
-            console.log('    </last_name>');
-            console.log('    <role type="author">');
-            console.log('  </user>');
+        authors.forEach(function (author){
+            
+                console.log('  <user>');
+                var tokens = tokenize(author.trim());
+                console.log('    <first_name>');
+                console.log('      ', tokens.first_name);
+                console.log('    </first_name>');
+                if (tokens.middle_name){
+                    console.log('    <middle_name>');
+                    console.log('      ', tokens.middle_name);
+                    console.log('    </middle_name>');
+                }
+                console.log('    <last_name>');
+                console.log('      ',tokens.last_name);
+                console.log('    </last_name>');
+                console.log('    <email>');
+                console.log('    </email>');
+                console.log('    <role type="author" />');
+                console.log('  </user>');
+            
         });
     });
 });
